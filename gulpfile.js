07aac9c43c7575f4bf58def68371bc9a014f9959
rouz majlessi <rmajlessi@gmail.com>
2016-08-20 00:41:39 -0700

    add bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
+var jshint = require('gulp-jshint');
+
 
 var buildProduction = utilities.env.production;
 
@@ -48,4 +50,14 @@ gulp.task("build", function(){
   } else {
     gulp.start('jsBrowserify');
   }
+});
+
+
+
+
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
