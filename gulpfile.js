c57e0167ff148eed947a8836505d9d5344eccf34
rouz majlessi <rmajlessi@gmail.com>
2016-08-20 14:44:13 -0700

    edit gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -93,6 +93,7 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
 });
 
 
