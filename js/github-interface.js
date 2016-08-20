0a114a651c6c041cb13712786757b570868ad3fb
rouz majlessi <rmajlessi@gmail.com>
2016-08-20 16:32:39 -0700

    add interface jquery and protoype in backend jscripts

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,1 +1,13 @@
-var Repo = require('./../js/github.js').gitModule;
+var Git= require('./../js/github.js').gitModule;
+
+$(document).ready(function() {
+  var.currentGit = new Git();
+  $('.search').submit(funtion(event) {
+    event.preventDefault();
+    var input = $('#search-user').val();
+    $('#search-user').val("");
+    currentGit.gitSearch(input);
+    console.log(input);
+
+  });
+});
--- js/github.js
+++ js/github.js
@@ -1,21 +1,14 @@
 var apiKey = require('./../.env').apiKey;
 
 
-function Repo(testRepo) {
-  this.repo = testRepo;
+function Git() {
 }
 
-Repo.prototype.test=function(test){
-  return test;
+Git.prototype.gitSearch=function(input){
+  $.get('https://api.github.com/users/' + input + '/repos?access_token=' + apiKey).then(function(response) {
+
 };
 
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
 
-exports.repoModule = Repo;
+exports.gitModule = Git;
