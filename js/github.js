46041dea9945767b1c94004d7046c4e49aaa9c70
rouz majlessi <rmajlessi@gmail.com>
2016-08-22 20:40:32 -0700

    add rep too backend

--- js/github.js
+++ js/github.js
@@ -6,8 +6,8 @@ function Repo() {
 Repo.prototype.getRepos = function(name) {
   console.log("ddd");
   console.log(name)
-  $.get('https://api.github.com/users/' + name +'?access_token=' + apiKey).then(function(response){
-    console.log(JSON.stringify(response.repos_url));
+  $.get('https://api.github.com/users/' + name + '/repos?/access_token=' + apiKey).then(function(response){
+    console.log(JSON.stringify(response));
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
